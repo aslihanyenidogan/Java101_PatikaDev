184e34b142c91a7ac433af061150912020762af4
Aslıhan Yenidoğan <106346858+aslihanyenidogan@users.noreply.github.com>
2022-07-25 22:21:43 +0300

    Add files via upload

---
+++ Dairenin_Alani_Ve_Cevresi/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int r,a;
+        double pi = 3.14;
+
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Dairenin yarı çapını giriniz : ");
+        r=inp.nextInt();
+        System.out.print("Dairenin merkez açısının ölçüsünü giriniz : ");
+        a=inp.nextInt();
+        double alan=(pi*(r*r)*a)/360;
+
+
+        System.out.println("Dairenin alanı : "+alan);
+
+    }
+}
